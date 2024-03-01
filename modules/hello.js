485be34ca8b80d52fb7b474e264ef87ab87f3ed3
hadiraza <hadiraza.edu@gmail.com>
2024-03-01 02:49:55 -0800

    module exports

--- modules/hello.js
+++ modules/hello.js
@@ -1,2 +1,13 @@
 const math = require("./math")
-console.log(math.add(4,8));
+console.log(math.addFn(4,8));
+
+const { sub, mul} = require("./subtract")
+console.log(sub(2,7));
+console.log(mul(2,7));
+
+const {multiply, addition} = require('./multiply');
+console.log(multiply(2,9));
+console.log(addition(2,9));
+
+const fs = require('fs')
+console.log(fs);
---
+++ modules/subtract.js
@@ -0,0 +1,8 @@
+function sub(a, b) {
+    return a - b;
+}
+function mul(a, b) {
+    return a * b;
+}
+
+module.exports = {sub, mul}
